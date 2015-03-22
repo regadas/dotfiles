1ce164decf79d5386c46a87c7941378e2063f9ec
regadas <filiperegadas@gmail.com>
2015-03-22 10:33:04 +0000

    Set hombrew url if it doesn't have a value

--- homebrew/install.mk
+++ homebrew/install.mk
@@ -1,4 +1,4 @@
-HOMEBREW_URL = "https://raw.githubusercontent.com/Homebrew/install/master/install"
+HOMEBREW_URL ?= https://raw.githubusercontent.com/Homebrew/install/master/install
 
 homebrew-install:
 	@test -e /usr/local/bin/brew || ruby -e "$$(curl -fsSL ${HOMEBREW_URL})"
