7510b673b70e0abc3cde6bacf6c3e14e06bfe937
regadas <filiperegadas@gmail.com>
2015-03-22 10:30:58 +0000

    Add spf13 vim distribution

--- vim/install.mk
+++ vim/install.mk
@@ -1,4 +1,11 @@
+SPF13_URL ?= https://j.mp/spf13-vim3
+
+all: vim-install spf13-install
+
 vim-install: homebrew-install
 	@test -e /usr/local/bin/vim || brew install vim
+
+spf13-install: vim-install
+	@curl -L $(SPF13_URL) | sh -
 
 include homebrew/install.mk
