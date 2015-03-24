409c3546c8af5d3566622245be5711c72308d248
Filipe Regadas <filipe.regadas@outplay.com>
2015-03-24 16:42:18 +0000

    Add simple ack config

--- ack/install.mk
+++ ack/install.mk
@@ -1,9 +1,15 @@
 
 all: ack
 
-ack: homebrew
+ack: ack-install ack-symlink
+
+ack-install: homebrew
 	@test -e /usr/local/bin/ack || brew install ack
 
-.PHONY: all ack
+ack-symlink:
+	@ln -sf $(MK_DIR)/ackrc $(HOME)/.ackrc
 
+.PHONY: all ack ack-install ack-symlink
+
+include base.mk
 include homebrew/install.mk
